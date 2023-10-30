ecc5ffe6f542eb1a57ed25519e719e07a7af6bb0
hvpexe <phuhuynh923@gmail.com>
2023-10-30 13:19:27 +0700

    fix UI buy ticket

--- src/pages/buy-ticket-template/CreateSuccess.js
+++ src/pages/buy-ticket-template/CreateSuccess.js
@@ -2,94 +2,95 @@ import { format } from "date-fns";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ApiImage } from "../../components/common/ApiImage";
+import moment from "moment/moment";
 
 function CreateSuccess({ paymentMethods, values }) {
-  const [loading, SetLoading] = useState(true);
-  useEffect(() => {
-    if (values) {
-      sessionStorage.setItem(`ticket${values.Code}`, JSON.stringify(values));
-      SetLoading(false);
-    } else SetLoading(true);
-  }, [values]);
-  return (
-    <section className="card card-compact w-full m-8 md:w-2/3 lg:w-3/5 max-w-[650px] bg-cor3 border-cor6 relative shadow  my-10">
-      <div className="card-body ">
-        <div className="card-title">Ticket Created</div>
-        {loading ? (
-          <div className=" loading h-[400px] m-auto"></div>
-        ) : (
-          <>
-            <div className=" flex flex-col px-16">
-              <div className="w-full text-center flex flex-col items-center border rounded p-6">
-                <ApiImage className={"w-[75px]"} value={values.Code} />
-                <span className="">
-                  <strong>Code:</strong> {values.Code}
-                </span>
-                <span className="py-6">
-                  <strong>CreationDate:</strong>{" "}
-                  {new Date(values.CreationDate).toString()}
-                </span>
-              </div>
-              <div className="flex justify-between my-2">
-                <div className="w-auto">Customer Name</div>
-                <div className="w-auto">{values.CustomerName}</div>
-              </div>
-              <div className="flex justify-between my-2">
-                <div className="w-auto">Phone Number</div>
-                <div className="w-auto">{values.PhoneNumber}</div>
-              </div>
-              <div className="flex justify-between my-2">
-                <div className="w-auto">Payment Method</div>
-                <div className="w-auto">{values.PaymentMethod}</div>
-              </div>
-              <div className="flex justify-between my-2">
-                <div className="w-auto">Effective Date</div>
-                <div className="w-auto">
-                  {format(new Date(values.EffectiveDate), "dd/MM/yyyy")}
-                </div>
-              </div>
-              <div className="flex justify-between my-2">
-                <div className="w-auto">Total Money</div>
-                <div className="w-auto">
-                  {values.TotalMoney?.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "VND",
-                  })}
-                </div>
-              </div>
-              <div className="flex justify-between my-2">
-                <div className="w-auto">Total Ticket</div>
-                <div className="w-auto">{values.TotalTicket}</div>
-              </div>
+	const [loading, SetLoading] = useState(true);
+	useEffect(() => {
+		if (values) {
+			sessionStorage.setItem(`ticket${values.Code}`, JSON.stringify(values));
+			SetLoading(false);
+		} else SetLoading(true);
+	}, [values]);
+	return (
+		<section className="card card-compact w-full m-8 md:w-2/3 lg:w-3/5 max-w-[650px] bg-cor4 relative shadow  my-10">
+			<div className="card-body ">
+				<div className="card-title text-cor2">Ticket Created</div>
+				{loading ? (
+					<div className=" loading h-[400px] m-auto"></div>
+				) : (
+					<>
+						<div className=" flex flex-col px-16 text-cor2 text-lg">
+							<div className="w-full text-center flex flex-col items-center bg-[#1b3323] rounded p-6">
+								<ApiImage className={"w-[200px]"} value={values.Code} />
+								<span className="mt-4">
+									<strong>Code:</strong> {values.Code}
+								</span>
+								<span className="mt-4">
+									<strong>CreationDate:</strong>{" "}
+									{moment(values.CreationDate).format("YYYY-MM-DD hh:mm:ss")}
+								</span>
+							</div>
+							<div className="flex justify-between my-2 mt-6">
+								<div className="w-auto">Customer Name</div>
+								<div className="w-auto">{values.CustomerName}</div>
+							</div>
+							<div className="flex justify-between my-2">
+								<div className="w-auto">Phone Number</div>
+								<div className="w-auto">{values.PhoneNumber}</div>
+							</div>
+							<div className="flex justify-between my-2">
+								<div className="w-auto">Payment Method</div>
+								<div className="w-auto">{values.PaymentMethod}</div>
+							</div>
+							<div className="flex justify-between my-2">
+								<div className="w-auto">Effective Date</div>
+								<div className="w-auto">
+									{format(new Date(values.EffectiveDate), "dd/MM/yyyy")}
+								</div>
+							</div>
+							<div className="flex justify-between my-2">
+								<div className="w-auto">Total Money</div>
+								<div className="w-auto">
+									{values.TotalMoney?.toLocaleString("en-US", {
+										style: "currency",
+										currency: "VND",
+									})}
+								</div>
+							</div>
+							<div className="flex justify-between my-2">
+								<div className="w-auto">Total Ticket</div>
+								<div className="w-auto">{values.TotalTicket}</div>
+							</div>
 
-              <hr></hr>
-              <div className="flex flex-col items-center p-6">
-                {paymentMethods.map((payment) => (
-                  <>
-                    {payment.value === values.PaymentMethod ? (
-                      <>
-                        {payment.display && (
-                          <div className="card-title">Confirm Payment</div>
-                        )}
-                        {payment.display}
-                      </>
-                    ) : null}
-                  </>
-                ))}
-              </div>
-              <span className="text-xs my-2 ms-[-20px]">
-                Ticket Information Has been Sent to your Email & Phone address
-              </span>
-            </div>
-          </>
-        )}
-        <Link to="/" className="btn">
-          Back To Home
-        </Link>
-        {/* Add other routes for your application */}
-      </div>
-    </section>
-  );
+							<hr></hr>
+							<div className="flex flex-col items-center p-6">
+								{paymentMethods.map((payment) => (
+									<>
+										{payment.value === values.PaymentMethod ? (
+											<>
+												{payment.display && (
+													<div className="card-title">Confirm Payment</div>
+												)}
+												{payment.display}
+											</>
+										) : null}
+									</>
+								))}
+							</div>
+							<span className="text-xs my-2 ms-[-20px]">
+								Ticket Information Has been Sent to your Email & Phone address
+							</span>
+						</div>
+					</>
+				)}
+				<Link to="/" className="btn">
+					Back To Home
+				</Link>
+				{/* Add other routes for your application */}
+			</div>
+		</section>
+	);
 }
 
 export default CreateSuccess;
--- src/pages/buy-ticket-template/CustomerSection.js
+++ src/pages/buy-ticket-template/CustomerSection.js
@@ -1,80 +1,82 @@
 import { Progress } from "./Progress";
-
+import {ArrowRightIcon} from "@heroicons/react/24/outline";
 function CustomerSection({
-  step,
-  totalStep,
-  nextStep,
-  handleChange,
-  values,
-  paymentMethods,
+	step,
+	totalStep,
+	nextStep,
+	handleChange,
+	values,
+	paymentMethods,
 }) {
-  return (
-    <form className="card card-compact w-full m-8 md:w-2/3 lg:w-3/5 max-w-[650px] bg-cor3 border-cor6  shadow  my-10 relative">
-      <div className="card-body ">
-        <div className="card-title">Your Information</div>
-        <Progress max={totalStep} value={step} />
+	return (
+		<form className="card card-compact w-full m-8 md:w-2/3 lg:w-3/5 max-w-[650px] bg-cor4 text-cor2 shadow relative">
+			<div className="card-body ">
+				<div className="card-title">Your Information</div>
+				<Progress max={totalStep} value={step} />
 
-        <div className="flex justify-between gap-6">
-          <div className="form-control w-1/2">
-            <label className="label">
-              <span className="label-text">Customer Name</span>
-            </label>
-            <input
-              className="input "
-              onChange={handleChange("customerName")}
-              name="customerName"
-              value={values.customerName}
-            />
-            <label className="label">
-              <span className="label-text-alt text-red-600"></span>
-            </label>
-          </div>
-          <div className="form-control inline-flex w-1/2">
-            <label className="label">
-              <span className="label-text">Phone Number</span>
-            </label>
-            <input
-              className="input"
-              onChange={handleChange("phoneNumber")}
-              name="phoneNumber"
-              value={values.phoneNumber}
-            />
-            <label className="label">
-              <span className="label-text-alt text-red-600"></span>
-            </label>
-          </div>
-        </div>
-        <div className="form-control inline-flex w-1/2">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            className="input"
-            onChange={handleChange("email")}
-            placeholder="Enter Your Email (optional)"
-            value={values.email}
-            name="email"
-          />
-          <label className="label">
-            <span className="label-text-alt text-red-600"></span>
-          </label>
-        </div>
-       
-        <div className="w-auto flex justify-between">
-          <button
-            className="btn ms-auto"
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              nextStep();
-            }}
-          >
-            {">"}
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+				<div className="flex justify-between gap-6">
+					<div className="form-control w-1/2">
+						<label className="label">
+							<span className="label-text text-inherit">Customer Name</span>
+						</label>
+						<input
+							className="input text-black"
+							placeholder="Enter Your Full Name"
+							onChange={handleChange("customerName")}
+							name="customerName"
+							value={values.customerName}
+						/>
+						<label className="label">
+							<span className="label-text-alt text-red-600"></span>
+						</label>
+					</div>
+					<div className="form-control inline-flex w-1/2">
+						<label className="label">
+							<span className="label-text text-inherit">Phone Number</span>
+						</label>
+						<input
+							className="input text-black"
+							placeholder="Enter Your Phone Number"
+							onChange={handleChange("phoneNumber")}
+							name="phoneNumber"
+							value={values.phoneNumber}
+						/>
+						<label className="label">
+							<span className="label-text-alt text-red-600"></span>
+						</label>
+					</div>
+				</div>
+				<div className="form-control inline-flex w-1/2">
+					<label className="label">
+						<span className="label-text text-inherit">Email</span>
+					</label>
+					<input
+						className="input text-black"
+						onChange={handleChange("email")}
+						placeholder="Enter Your Email (optional)"
+						value={values.email}
+						name="email"
+					/>
+					<label className="label">
+						<span className="label-text-alt text-red-600"></span>
+					</label>
+				</div>
+
+				<div className="w-auto flex justify-between">
+					<button
+						className="btn ms-auto"
+						type="submit"
+						onClick={(e) => {
+							e.preventDefault();
+							nextStep();
+						}}
+					>
+						<ArrowRightIcon className="h-6 w-6 stroke-2"/>
+					</button>
+				</div>
+			</div>
+		</form>
+	);
 }
 
 export default CustomerSection;
